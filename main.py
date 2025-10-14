0044bd24cf0b56f2409ed7b00626e8e950f48364
AliBars19 <Ali.Bars@city.ac.uk>
2025-10-14 20:10:20 +0100

    important notes

---
+++ main.py
@@ -0,0 +1,16 @@
+#General MCAP Format (for reference(help i wanna kms))
+
+#Header
+#Schema
+#Channel
+#Message
+#Footer
+
+#Were gna make the fake/raw data in JSON format
+
+#ID: 501 , GPS (lat/long)
+#ID: 502 , IMU (accel,speed)
+
+#IMPORTANT, CAN SENDS TWO AT A TIME SO MAKE SURE TO PAIR THEM
+
+
